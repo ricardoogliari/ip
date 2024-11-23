779ffd6dadd397eae888d786773639ed1e71cbe1
rogliariping <rogliariping@gmail.com>
2024-11-23 16:55:57 +0000

    online using getx - done

--- lib/widgets/online.dart
+++ lib/widgets/online.dart
@@ -1,21 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+import 'package:myapp/controllers/db_controller.dart';
+import 'package:myapp/controllers/rest_controller.dart';
 import 'package:myapp/main.dart';
 import 'package:myapp/source/local/database.dart';
 import 'package:myapp/model/geo_data.dart' as model;
-import 'package:myapp/source/remote/rest_client.dart';
 
-class Online extends StatelessWidget {
+class Online extends GetView<RestController> {
   Online({super.key});
 
   final TextEditingController _ipController = TextEditingController();
 
-  final client = RestClient(Dio());
-  final database = AppDatabase();
-
   @override
   Widget build(BuildContext context) {
-
     return Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
@@ -29,52 +26,40 @@ class Online extends StatelessWidget {
             ),
             TextButton(
                 onPressed: () async {
-                  model.GeoData geo = await client.getGeoData(_ipController.text);
-                  showBottomSheet(
-                    enableDrag: true,
-                    showDragHandle: true,
-                    context: context,
-                    builder: (context) {
-                      return Wrap(
-                        children: [
-                          ListTile(
-                            title: Text('City: ${geo.city}'),
-                          ),
-                          ListTile(
-                            title: Text('Region Name: ${geo.regionName}'),
-                          ),
-                          ListTile(
-                            title: Text('Country: ${geo.country}'),
-                          ),
-                          IconButton(
-                              onPressed: () {
-                                Navigator.pushNamed(context, mapRoute,
-                                    arguments: {
-                                      "lat": geo.lat,
-                                      "lon": geo.lon
-                                    });
-                              },
-                              icon: const Icon(Icons.map_outlined))
-                        ],
-                      );
-                    },
-                  );
-
-                  _save(data: geo);
+                  controller.getGeoIp(ip: _ipController.text);
                 },
-                child: const Text("Find"))
-          ],
-        ));
-  }
+                child: const Text("Find")),
+            controller.obx(
+              (geo) {
+                Get.find<DbController>().insertData(geo: geo);
 
-  void _save({required model.GeoData data}) {
-    database.into(database.geoData).insert(GeoDataCompanion.insert(
-          ip: data.query ?? "",
-          city: data.city ?? "",
-          regionName: data.regionName ?? "",
-          country: data.country ?? "",
-          lat: "${data.lat}",
-          lon: "${data.lon}",
+                return Card(
+                  child: Wrap(
+                    children: [
+                      ListTile(
+                        title: Text('City: ${geo?.city}'),
+                      ),
+                      ListTile(
+                        title: Text('Region Name: ${geo?.regionName}'),
+                      ),
+                      ListTile(
+                        title: Text('Country: ${geo?.country}'),
+                      ),
+                      IconButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, mapRoute,
+                                arguments: {"lat": geo?.lat, "lon": geo?.lon});
+                          },
+                          icon: const Icon(Icons.map_outlined))
+                    ],
+                  ),
+                );
+              },
+              onEmpty: null,
+              onLoading: const CircularProgressIndicator(),
+              onError: (error) => Text(error ?? "Erro inesperado!"),
+            )
+          ],
         ));
   }
 }
